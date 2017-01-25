5a06745102a611c3783ea4150488cad31fc9d405
balkhaev <mike@onlinesup.com>
2017-01-25 13:47:37 +0300

    small changes

--- logger.js
+++ logger.js
@@ -1,60 +1,61 @@
-var colors = {
-	browser: ['color: #1cb329', 'color: #ff0', 'color: #5454ff', 'color: #f0f'],
-	node: ['\x1b[32m%s\x1b[0m', '\x1b[33m%s\x1b[0m', '\x1b[34m%s\x1b[0m', '\x1b[35m%s\x1b[0m']
-}
+(function() {
+	var colors = {
+		browser: ['color: #1cb329', 'color: #ff0', 'color: #5454ff', 'color: #f0f'],
+		node: ['\x1b[32m%s\x1b[0m', '\x1b[33m%s\x1b[0m', '\x1b[34m%s\x1b[0m', '\x1b[35m%s\x1b[0m']
+	};
 
-var isBrowser = new Function('try {return this===window;}catch(e){ return false;}');
-var isNode = new Function('try {return this===global;}catch(e){ return false;}');
-
-function newLogger(loggerName) {
-	var title = loggerName || 'logger'
-	var color = {
-		node: colors.node.pop(),
-		browser: colors.browser.pop()
-	}
+	var isBrowser = new Function('try {return this===window;}catch(e){ return false;}');
+	var isNode = new Function('try {return this===global;}catch(e){ return false;}');
 
 	function isDebug() {
 		return (isBrowser() && window.location.hash.indexOf('debug=enable') !== -1)
 			|| (isNode() && (process.env.debug === '1' || process.argv.indexOf('-d') !== -1))
 	}
 
-	function getMsg(args) {
-		var msg = Array.prototype.slice.call(args) || []
+	function newLogger(loggerName) {
+		var title = loggerName || 'logger';
+		var color = isBrowser() ? colors.browser.pop() :  colors.node.pop();
 
-		if (isBrowser()) {
-			msg.unshift('%c ' + title, color.browser)
-		} else {
-			msg.unshift(color.node, title)
+		function getMsg(args) {
+			var msg = Array.prototype.slice.call(args) || [];
+
+			if (isBrowser()) {
+				msg.unshift('%c ' + title, color);
+			} else {
+				msg.unshift(color, title);
+			}
+
+			return msg;
 		}
 
-		return msg
-	}
+		function logger() {
+			if (!isDebug()) {
+				return;
+			}
 
-	function logger() {
-		if (!isDebug()) {
-			return
+			console.log.apply(console, getMsg(arguments));
 		}
 
-		console.log.apply(console, getMsg(arguments))
-	}
+		logger.warning = function() {
+			if (!isDebug()) {
+				return;
+			}
 
-	logger.warning = function() {
-		if (!isDebug()) {
-			return
+			console.warn.apply(console, getMsg(arguments));
 		}
 
-		console.warn.apply(console, getMsg(arguments))
-	}
+		logger.error = function() {
+			if (!isDebug()) {
+				return;
+			}
 
-	logger.error = function() {
-		if (!isDebug()) {
-			return
+			console.error.apply(console, getMsg(arguments));
 		}
 
-		console.error.apply(console, getMsg(arguments))
+		return logger;
 	}
 
-	return logger
-}
-
-module.exports = newLogger
+	if (typeof module !== 'undefined') {
+		module.exports = newLogger;
+	}
+})();
